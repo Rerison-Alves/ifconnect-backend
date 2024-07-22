9b30416cebb8f0e4071b51c7d5a695425541d7a0
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2024-07-22 12:53:52 -0300

    Correção curso do aluno

--- src/main/java/com/ifconnect/ifconnectbackend/models/Aluno.java
+++ src/main/java/com/ifconnect/ifconnectbackend/models/Aluno.java
@@ -1,21 +1,22 @@
 package com.ifconnect.ifconnectbackend.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 
 @Embeddable
+@Getter
+@Setter
 public class Aluno {
 
     @ManyToOne
-    @JoinColumn(name = "id_curso", referencedColumnName = "id")
+    @JoinColumn(name = "id_curso")
+    @JsonIgnoreProperties("iconeBase64")
     private Curso curso;
 
-    @Getter
     @Column(unique = true)
     @NotBlank(message = "Matrícula ${notblank}")
     @Size(min = 14, max = 14, message = "Matrícula ${exactsize}")
